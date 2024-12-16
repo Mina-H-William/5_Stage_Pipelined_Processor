68aa7f2364ae9c6c56f7f26b9d4acb8ded2e8262
MHW <118685507+Monwilliam@users.noreply.github.com>
2024-12-16 11:15:31 +0200

    Add initial project files and configurations

---
+++ main.vhd
@@ -0,0 +1,21 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity main is
+    generic (
+
+    );
+    Port (  
+   
+    );
+end main;
+
+architecture Behavioral of main is
+
+begin
+
+
+end Behavioral;
+
+

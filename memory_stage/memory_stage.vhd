22a053f624f0081b4a48bcad11a4d93fe9961f9a
ali bahr <alibahr313@gmail.com>
2024-12-22 02:09:37 +0200

    refactor: refactored the memeory stage

--- memory_stage/memory_stage.vhd
+++ memory_stage/memory_stage.vhd
@@ -3,39 +3,24 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY memory_stage IS
-    GENERIC (
-        reg_size : INTEGER := 16;
-        memory_address_size : INTEGER := 12);
     PORT (
-        clk : IN STD_LOGIC;
-        sp_write_signal : IN STD_LOGIC;
-        int_signal : IN STD_LOGIC;
-        rti_signal : IN STD_LOGIC;
-        call_signal : IN STD_LOGIC;
-        mem_write_signal : IN STD_LOGIC;
-        mem_read_signal : IN STD_LOGIC;
-        pc : IN STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
-        alu_output : IN STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
-        alu_input_2 : IN STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
-        flags : IN STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
-        sp : IN STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
-        data_out : OUT STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
+        clk                         : IN STD_LOGIC;
+        sp_write_signal             : IN STD_LOGIC;
+        int_signal_from_meomery     : IN STD_LOGIC;
+        int_signal_from_write_back  : IN STD_LOGIC;
+        rti_signal_from_write_back  : IN STD_LOGIC;
+        call_signal                 : IN STD_LOGIC;
+        mem_write_signal            : IN STD_LOGIC;
+        mem_read_signal             : IN STD_LOGIC;
+        pc                          : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        alu_output                  : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        alu_input_2                 : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        flags                       : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+        sp                          : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+        sp_write_back               : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
 
-        sp_out : OUT STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
-        alu_output_out : OUT STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
-        memory_address_out : OUT STD_LOGIC_VECTOR(memory_address_size - 1 DOWNTO 0);
-
-        r_dest_in : IN STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
-        r_dest_out : OUT STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
-
-        int_signal_out : OUT STD_LOGIC;
-        rti_signal_out : OUT STD_LOGIC;
-        ret_or_rti_signal_in : IN STD_LOGIC; -- Added as input
-        ret_or_rti_signal_out : OUT STD_LOGIC; -- Added as output
-        req_write_signal_in : IN STD_LOGIC; -- Added as input
-        req_write_signal_out : OUT STD_LOGIC; -- Added as output
-        mem_to_reg_signal_in : IN STD_LOGIC; -- Added as input
-        mem_to_reg_signal_out : OUT STD_LOGIC -- Added as output
+        data_out                    : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+        memory_address_out          : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
 
     );
 END ENTITY memory_stage;
@@ -50,8 +35,8 @@ ARCHITECTURE memory_arch OF memory_stage IS
             input_1 : IN STD_LOGIC_VECTOR(size - 1 DOWNTO 0);
             input_2 : IN STD_LOGIC_VECTOR(size - 1 DOWNTO 0);
             input_3 : IN STD_LOGIC_VECTOR(size - 1 DOWNTO 0);
-            sel : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
-            result : OUT STD_LOGIC_VECTOR(size - 1 DOWNTO 0)
+            sel     : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+            result  : OUT STD_LOGIC_VECTOR(size - 1 DOWNTO 0)
         );
     END COMPONENT;
 
@@ -62,8 +47,8 @@ ARCHITECTURE memory_arch OF memory_stage IS
         PORT (
             input_0 : IN STD_LOGIC_VECTOR(size - 1 DOWNTO 0);
             input_1 : IN STD_LOGIC_VECTOR(size - 1 DOWNTO 0);
-            sel : IN STD_LOGIC;
-            result : OUT STD_LOGIC_VECTOR(size - 1 DOWNTO 0)
+            sel     : IN STD_LOGIC;
+            result  : OUT STD_LOGIC_VECTOR(size - 1 DOWNTO 0)
         );
     END COMPONENT;
 
@@ -73,51 +58,46 @@ ARCHITECTURE memory_arch OF memory_stage IS
             ADDRESS_WIDTH : INTEGER
         );
         PORT (
-            clk : IN STD_LOGIC;
-            we : IN STD_LOGIC;
-            re : IN STD_LOGIC;
+            clk     : IN STD_LOGIC;
+            we      : IN STD_LOGIC;
+            re      : IN STD_LOGIC;
             address : IN STD_LOGIC_VECTOR(ADDRESS_WIDTH - 1 DOWNTO 0);
             data_in : IN STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
-            data_out : OUT STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0)
+            data_out: OUT STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0)
         );
     END COMPONENT;
 
     -- Internal signals
-    SIGNAL pc_incremented : STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
-    SIGNAL pc_decremented : STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
+    SIGNAL pc_incremented : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL sp_decremented : STD_LOGIC_VECTOR(15 DOWNTO 0);
     SIGNAL int_or_rti : STD_LOGIC;
     SIGNAL int_or_call : STD_LOGIC;
-    SIGNAL mux01_input_0 : STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
+    SIGNAL mux01_input_0 : STD_LOGIC_VECTOR(15 DOWNTO 0);
     SIGNAL mux10_sel : STD_LOGIC_VECTOR(1 DOWNTO 0);
 
-    SIGNAL memory_address : STD_LOGIC_VECTOR(memory_address_size - 1 DOWNTO 0);
-    SIGNAL memory_data_in : STD_LOGIC_VECTOR(reg_size - 1 DOWNTO 0);
+    SIGNAL expanded_flags : STD_LOGIC_VECTOR(15 DOWNTO 0); 
+
+    SIGNAL memory_address : STD_LOGIC_VECTOR(15 DOWNTO 0);
+    SIGNAL memory_data_in : STD_LOGIC_VECTOR(15 DOWNTO 0);
     SIGNAL memory_we : STD_LOGIC;
     SIGNAL memory_re : STD_LOGIC;
 BEGIN
 
-    -- signals to bypass 
-    ret_or_rti_signal_out <= ret_or_rti_signal_in;
-    req_write_signal_out <= req_write_signal_in; -- Added to bypass section
-    mem_to_reg_signal_out <= mem_to_reg_signal_in; -- Added to bypass section
-    rti_signal_out <= rti_signal;
-    int_signal_out <= int_signal;
-    sp_out <= sp;
-    alu_output_out <= alu_output;
-    r_dest_out <= r_dest_in;
+    
     memory_address_out <= memory_address;
-
+    
+    expanded_flags <= (15 DOWNTO 3 => '0') & flags;
     -- Increment and decrement the program counter
     pc_incremented <= STD_LOGIC_VECTOR(unsigned(pc) + 1);
-    pc_decremented <= STD_LOGIC_VECTOR(unsigned(pc) - 1);
+    sp_decremented <= STD_LOGIC_VECTOR(unsigned(sp_write_back) - 1);
 
     -- Combine interrupt and return from interrupt signals
-    int_or_rti <= rti_signal OR int_signal;
-    int_or_call <= call_signal OR int_signal;
+    int_or_rti <= rti_signal_from_write_back OR int_signal_from_write_back;
+    int_or_call <= call_signal OR int_signal_from_meomery;
 
     -- Memory write and read enable signals
-    memory_we <= mem_write_signal OR int_signal;
-    memory_re <= mem_read_signal OR rti_signal;
+    memory_we <= mem_write_signal OR int_signal_from_write_back;
+    memory_re <= mem_read_signal OR rti_signal_from_write_back;
 
     -- Multiplexer for memory address selection 
     mux10_sel <= sp_write_signal & int_or_rti;
@@ -125,17 +105,17 @@ BEGIN
     memory_address_mux : mux_4_input
     GENERIC MAP(size => 16)
     PORT MAP(
-        input_0 => alu_output(memory_address_size - 1 DOWNTO 0),
-        input_1 => alu_output(memory_address_size - 1 DOWNTO 0),
-        input_2 => sp(memory_address_size - 1 DOWNTO 0),
-        input_3 => pc_decremented(memory_address_size - 1 DOWNTO 0),
+        input_0 => alu_output,
+        input_1 => alu_output,
+        input_2 => sp,
+        input_3 => sp_decremented,
         sel => mux10_sel,
         result => memory_address
     );
 
     -- Multiplexer for selecting input to the next stage
     mux00 : mux_2_input
-    GENERIC MAP(size => reg_size)
+    GENERIC MAP(size => 16)
     PORT MAP(
         input_0 => alu_input_2,
         input_1 => pc_incremented,
@@ -145,19 +125,19 @@ BEGIN
 
     -- Multiplexer for selecting data to write to memory
     mux01 : mux_2_input
-    GENERIC MAP(size => reg_size)
+    GENERIC MAP(size => 16)
     PORT MAP(
         input_0 => mux01_input_0,
-        input_1 => flags,
-        sel => int_signal,
+        input_1 => expanded_flags,
+        sel => int_signal_from_write_back,
         result => memory_data_in
     );
 
     -- RAM instantiation
     ram_port_map : ram
     GENERIC MAP(
-        DATA_WIDTH => reg_size,
-        ADDRESS_WIDTH => memory_address_size
+        DATA_WIDTH => 16,
+        ADDRESS_WIDTH => 16
     )
     PORT MAP(
         clk => clk,
